4e80ffc4b345d6fbf5cfa047d1510d47a81b26f5
suron <suron.maharjan@gmail.com>
2024-05-14 21:43:22 +0545

    add new alert

--- src/App.jsx
+++ src/App.jsx
@@ -38,7 +38,7 @@ function App() {
 
   // will add the item in the cart when "Add" button is clicked
   const handleClick = (e) => {
-    if (item) {
+    if (item.trim() !== "") {
       addShoppingList(item);
       setItem("");
     }
@@ -85,7 +85,41 @@ function App() {
           <p className="mb-4">No List Found!!</p>
         )}
 
-        {alert && alert}
+        {alert && (
+          <div role="alert">
+            <div className="bg-red-500 text-white font-bold rounded-t px-4 py-2">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                enablebackground="new 0 0 47.5 47.5"
+                viewBox="0 0 47.5 47.5"
+                width="24"
+                height="24"
+                id="warning"
+              >
+                <defs>
+                  <clipPath id="a">
+                    <path d="M0 38h38V0H0v38Z" />
+                  </clipPath>
+                </defs>
+                <g clippath="url(#a)" transform="matrix(1.25 0 0 -1.25 0 47.5)">
+                  <path
+                    fill="#ffcc4d"
+                    d="M0 0c-1.842 0-2.654 1.338-1.806 2.973l15.609 30.055c.848 1.635 2.238 1.635 3.087 0L32.499 2.973C33.349 1.338 32.536 0 30.693 0H0Z"
+                    transform="translate(3.653 2)"
+                  />
+                  <path
+                    fill="#231f20"
+                    d="M0 0c0 1.302.961 2.108 2.232 2.108 1.241 0 2.233-.837 2.233-2.108v-11.938c0-1.271-.992-2.108-2.233-2.108-1.271 0-2.232.807-2.232 2.108V0Zm-.187-18.293a2.422 2.422 0 0 0 2.419 2.418 2.422 2.422 0 0 0 2.419-2.418 2.422 2.422 0 0 0-2.419-2.419 2.422 2.422 0 0 0-2.419 2.419"
+                    transform="translate(16.769 26.34)"
+                  />
+                </g>
+              </svg>
+            </div>
+            <div className="border border-t-0 border-red-400 rounded-b bg-red-100 px-4 py-3 text-red-700">
+              <p>{alert}</p>
+            </div>
+          </div>
+        )}
 
         {/* <ShoppingListItem /> */}
       </div>
