f80464681c7d2f9ef8773decd861a0a287f84aa2
suron <suron.maharjan@gmail.com>
2024-05-14 06:19:42 +0545

    counter-app solved

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
+import { useState } from "react";
 import "./App.css";
 import Child from "./components/Child";
 
 const App = () => {
+  const [count, setCount] = useState(0)
   return (
     <div className="App">
-      <h1>0</h1>
-      <Child />
-      <p>Why so negative?</p>
+      <h1>{count}</h1>
+      <Child setCount={setCount} />
+      { count < 0 ? <p>Why so negative?</p> : ''}
+      {/* <p>{message}</p> */}
     </div>
   );
 };
--- src/components/Child.jsx
+++ src/components/Child.jsx
@@ -1,10 +1,17 @@
-const Child = () => {
+import { useState } from "react";
+
+const Child = ({setCount}) => {
+  // const [count, setCount] = useState(0)
+
   const handleMinusClick = () => {
     /* Implement logic here */
+    setCount((prev) => prev-1)
+    
   };
 
   const handlePlusClick = () => {
     /* Implement logic here */
+    setCount((prev) => prev+1)
   };
 
   return (
@@ -15,6 +22,7 @@ const Child = () => {
       <button type="button" className="btn" onClick={handlePlusClick}>
         +
       </button>
+      
     </div>
   );
 };
