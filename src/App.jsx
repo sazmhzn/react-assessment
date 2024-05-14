bb566ae3ed23963506cd103061eeba3ffe83252f
suron <suron.maharjan@gmail.com>
2024-05-14 21:20:02 +0545

    shopping-list completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,55 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 import "./App.css";
 import { ShoppingListItem } from "./components/ShoppingListItem";
 
 function App() {
   const [shoppingList, setShoppingList] = useState([]);
-  const [item, setItem] = useState(null);
-
-  //Do something when button is clicked
-  const handleClick = () => {
-    addShoppingList(item);
-    console.log(item, "onclick"); 
-    console.log(shoppingList); 
-  };
-
+  const [item, setItem] = useState("");
+  const [alert, setAlert] = useState("");
 
   // Get the value from the input
   const handleChange = (e) => {
-    // setItem(e.target.value);
-    setItem(  () =>  e.target.value )
+    setItem(e.target.value);
   };
 
   const addShoppingList = (item) => {
-    //set the shopping list
-    setShoppingList([...shoppingList, {
+    const newList = {
       id: uuidv4(),
       name: item,
-    }]);
+    };
+
+    shoppingList.map((list) => {
+      //checking if the duplicate value exit in the array
+      if (list.name === item) {
+        setAlert("Item already exists in the list");
+        //tried to generate the alert pop up but is preventing the user from adding the value
+        alert("stop");
+      } else {
+        setAlert("");
+      }
+    });
+
+    //set the shopping list
+    setShoppingList((prev) => {
+      return [...prev, newList];
+    });
   };
 
-  // const deleteShoppingList = (item) => {
-  //   console.log("delete pressed for: ", item);
-  //   setShoppingList(shoppingList.filter((list) => list.id !== item.id));
-  // };
+  // will add the item in the cart when "Add" button is clicked
+  const handleClick = (e) => {
+    if (item) {
+      addShoppingList(item);
+      setItem("");
+    }
+  };
+
+  //delete the shopping list item
+  const deleteShoppingList = (id) => {
+    console.log("deleting for: ", id);
+    console.log("shoppingList: ", shoppingList);
+    setShoppingList(shoppingList.filter((list) => list.id !== id));
+  };
 
   return (
     <div className="container">
@@ -43,8 +60,8 @@ function App() {
           type="text"
           placeholder="E.g. Carrots"
           className="v__input flex-1"
+          value={item}
           onChange={handleChange}
-          // onChange={ (e) => setItem( e.target.value ) }
         />
         <button className="v__button" onClick={handleClick}>
           Add
@@ -55,11 +72,20 @@ function App() {
 
         {shoppingList.length > 0 ? (
           shoppingList.map((item) => {
-            return (<ShoppingListItem name={item.name}/> );
+            return (
+              <ShoppingListItem
+                name={item.name}
+                key={item.id}
+                id={item.id}
+                deleteShoppingList={deleteShoppingList}
+              />
+            );
           })
         ) : (
           <p className="mb-4">No List Found!!</p>
         )}
+
+        {alert && alert}
 
         {/* <ShoppingListItem /> */}
       </div>
--- src/components/ShoppingListItem.jsx
+++ src/components/ShoppingListItem.jsx
@@ -1,20 +1,26 @@
 import { useState } from "react";
 import classes from "./ShoppingListItem.module.css";
 
-export const ShoppingListItem = ({name}) => {
-
+export const ShoppingListItem = ({ name, id, deleteShoppingList }) => {
   const [checked, setChecked] = useState(false);
+
+  //will render the text to have a strikethrough effect
   const handleChange = () => {
     setChecked((prevState) => !prevState);
-  }
+  };
 
-  console.log(checked , name);  
-  
+
+
   return (
     <div className="flex items-center p-2">
-      <input type="checkbox" className="mr-2" checked={checked} onChange={handleChange} />
-      <h3 className={ checked ? "flex-1 line-through" : "flex-1" } >{name}</h3>
-      <button className={classes.removeButton}>x</button>
+      <input
+        type="checkbox"
+        className="mr-2"
+        checked={checked}
+        onChange={handleChange}
+      />
+      <h3 className={checked ? "flex-1 line-through" : "flex-1"}>{name}</h3>
+      <button className={classes.removeButton} onClick={ () =>  deleteShoppingList(id)}>x</button>
     </div>
   );
 };
